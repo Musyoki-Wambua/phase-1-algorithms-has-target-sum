3d1b57c58c133a27e30feebc10504e9b40b7e292
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2022-12-14 11:01:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  //iterate trhough the whole array
+  for(let i=0; i < array.length; i++){
+    //continue iteration to check if number is equal to that of x 
+    for (let j= i+1; j < array.length; j++){
+      if (array[i]+ array [j] === target){
+        return true}
+        //if not continue iterating
+    }
+    }
+      //return false if no answer =x
+  return false;
+  }
 
 /* 
   Write the Big O time complexity of your function here
+  Complexity is O(n*) since nested iterations are involved => 0(n)
 */
 
 /* 
   Add your pseudocode here
+  Iterate from the begging of the array to and log each item.
+  
+
+  
 */
 
 /*
   Add written explanation of your solution here
+ Go through the whole array starting at the array initializer index 0 through to when the stopping
+  condition is met, if not iteration continues until condition met is a boolean value
 */
 
 // You can run `node index.js` to view these console logs
